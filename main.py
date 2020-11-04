8a05abaa247671180457573031a4a91925b78dd1
VeronikaS21 <veronikaisles@gmail.com>
2020-11-04 11:33:49 +0100

    Added a bird and a rectangle around it

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ clock = pygame.time.Clock()
 bg_surface = pygame.transform.scale2x(pygame.image.load('assets/sprites/background-day.png').convert()) # to run the game faster we added convert
 floor_surface = pygame.transform.scale2x(pygame.image.load('assets/sprites/base.png').convert())
 floor_x_pos = 0
+# bird surface
+bird_surface = pygame.transform.scale2x(pygame.image.load('assets/sprites/bluebird-midflap.png').convert())
+bird_rect = bird_surface.get_rect(center = (100, 512))
 # Game loop
 while True:
   # to close the game
@@ -20,11 +23,12 @@ while True:
           sys.exit()
    #background
   screen.blit(bg_surface, (0,0)) # on display surface put the background surface
+  screen.blit(bird_surface, bird_rect)
   #floor, the image is never static it's always redrawn
   floor_x_pos -= 1
   draw_floor()
   if floor_x_pos <= -576:
       floor_x_pos = 0
-      
+
   pygame.display.update()
   clock.tick(120)
