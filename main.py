aa171a902e70f6e1fd595d92eabdc60d590224a2
VeronikaS21 <veronikaisles@gmail.com>
2020-11-04 11:05:52 +0100

    Added floor

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ pygame.init() #initializes pygame
 screen = pygame.display.set_mode((576, 1024)) # display surface, widht and height
 clock = pygame.time.Clock()
  #added surface
-bg_surface = pygame.transform.scale2x(pygame.image.load('assets/sprites/background-day.png').convert())
-
+bg_surface = pygame.transform.scale2x(pygame.image.load('assets/sprites/background-day.png').convert()) # to run the game faster we added convert
+floor_surface = pygame.transform.scale2x(pygame.image.load('assets/sprites/base.png').convert())
 # Game loop
 while True:
   # to close the game
@@ -14,5 +14,6 @@ while True:
           pygame.quit()
           sys.exit()
   screen.blit(bg_surface, (0,0)) # on display surface put the background surface
+  screen.blit(floor_surface, (0,(900)))
   pygame.display.update()
   clock.tick(120)
